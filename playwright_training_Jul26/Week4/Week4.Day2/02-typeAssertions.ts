fa21ff760c06493a9ea28ffc97d8765aa0314236
ravindrantest <ravindran.ramdas@testleaf.com>
2026-08-08 11:40:29 +0530

    Add files via upload

---
+++ playwright_training_Jul26/Week4/Week4.Day2/02-typeAssertions.ts
@@ -0,0 +1,9 @@
+
+let x : string = "username"
+
+// as or <> for typeAssertion
+
+/* when the username comes from outside source like .env where we manage all our credentials */
+
+/* await page.locator("#username").fill(username as string )  //  username should be string
+await page.locator("#password").fill(pwd)*/
